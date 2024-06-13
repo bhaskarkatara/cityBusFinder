4fb694542765ecb44a01eab04edc6e9c7a4eb9eb
bhaskar katara <bhaskarkatararjo5@gmail.com>
2024-06-13 16:41:03 +0530

    add data.json file#13-06-24

--- app/src/main/java/com/example/citybusfinder/ScreenFirst.kt
+++ app/src/main/java/com/example/citybusfinder/ScreenFirst.kt
@@ -14,8 +14,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-//import kotlinx.coroutines.flow.internal.NoOpContinuation.context
-//import kotlin.coroutines.jvm.internal.CompletedContinuation.context
+
 
 @Composable
 fun WelcomeScreen(navController: NavController,locationUtils: LocationUtils,context: Context) {
@@ -36,7 +35,7 @@ fun WelcomeScreen(navController: NavController,locationUtils: LocationUtils,cont
             )
         Spacer(modifier = Modifier.padding(top = 10.dp))
         Button(onClick = {
-            // agar user ne phele se hi permission de rhki h to direct third screen pe jaap
+            // agar user ne pehele se hi permission de rhki h to direct third screen pe jaap
             if(locationUtils.hasPermissionGranted(context)){
             navController.navigate(Screen.Finder.route)}
             else{
--- app/src/main/java/com/example/citybusfinder/ScreenThird.kt
+++ app/src/main/java/com/example/citybusfinder/ScreenThird.kt
@@ -5,7 +5,8 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @Composable
-
+//Todo : implement :- show the curr_coordinates of an user in the map
+// yes! Implement the fea: to search a bus by inputting the source and destination
 fun FinderScreen(navController: NavController){
     Text(text = "Welcome to MainScreen")
 }
