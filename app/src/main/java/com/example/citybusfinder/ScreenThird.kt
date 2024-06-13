b93b2f9d5dd630eac0810b564ef835de2021f842
bhaskar katara <bhaskarkatararjo5@gmail.com>
2024-06-13 21:57:02 +0530

    make Inputs filed#13-06-24

--- app/src/main/java/com/example/citybusfinder/ScreenThird.kt
+++ app/src/main/java/com/example/citybusfinder/ScreenThird.kt
@@ -1,12 +1,51 @@
 package com.example.citybusfinder
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
 //Todo : implement :- show the curr_coordinates of an user in the map
 // yes! Implement the fea: to search a bus by inputting the source and destination
 fun FinderScreen(navController: NavController){
-    Text(text = "Welcome to MainScreen")
+    var sourceInput by remember { mutableStateOf("") }
+    var destinationInput by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        OutlinedTextField(
+            value = sourceInput,
+            onValueChange = { sourceInput = it },
+            label = { Text(text = "Enter your source")
+
+             }
+        )
+        Spacer(modifier = Modifier.height(16.dp)) // Add spacing between the text fields
+        OutlinedTextField(
+            value = destinationInput,
+            onValueChange = { destinationInput = it },
+            label = { Text(text = "Enter your destination") }
+        )
+    }
+
+
+
 }
