2197935fff96cb74de700ef7cb14281b0c7a3d40
bhaskar katara <bhaskarkatararjo5@gmail.com>
2024-07-09 13:57:13 +0530

    Change json(names) to GoogleMaps Places

--- app/src/main/java/com/example/citybusfinder/ScreenThird.kt
+++ app/src/main/java/com/example/citybusfinder/ScreenThird.kt
@@ -407,7 +407,7 @@ private fun getDirectionsUrl(origin: LatLng, destination: LatLng): String {
     val originStr = "origin=${origin.latitude},${origin.longitude}"
     val destinationStr = "destination=${destination.latitude},${destination.longitude}"
     val mode = "mode=driving"
-    val parameters = "$originStr&$destinationStr&$mode&key=---------------"
+    val parameters = "$originStr&$destinationStr&$mode&key=------------"
     return "https://maps.googleapis.com/maps/api/directions/json?$parameters"
 }
 private fun decodePolyline(encoded: String): List<LatLng> {
